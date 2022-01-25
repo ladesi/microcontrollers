57dbc12809561100c4f16ae060f28c2a9655e3eb
david <davidcoelho89@gmail.com>
2022-01-25 15:16:31 -0300

    Initial Project (Hello World)
    Description: added Initial Project (Hello World)
    
    Commit: Add
    
    TaskNumber:#0

---
+++ initial_pic16f877a/firmware/init_pic16f877a_mplabx_v5.50_xc8_v2.32.X/main.c
@@ -0,0 +1,24 @@
+/*
+ * PIC16F877A Samples
+ * Sample 01: Initial (Hello World)
+ * Author: David Nascimento Coelho
+ * Last Update: 2022/01/25
+ */
+
+// LIBRARIES INCLUDE
+
+#include <xc.h>
+#include "main.h"
+
+// FUNCTIONS
+
+void main(void) 
+{
+	// Infinity loop
+	while(1)
+    {
+        
+    }
+	
+    return;
+}
---
+++ initial_pic16f877a/firmware/init_pic16f877a_mplabx_v5.50_xc8_v2.32.X/main.h
@@ -0,0 +1,29 @@
+/*
+ * PIC16F877A Samples
+ * Sample 01: Initial (Hello World)
+ * Author: David Nascimento Coelho
+ * Last Update: 2022/01/25
+ */
+
+#ifndef GLOBALS_H
+#define	GLOBALS_H
+#endif	/* GLOBALS_H */
+
+// CONFIGURATION BITS
+
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = ON         // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3/PGM pin has PGM function; low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// CONSTANTS
+
+#define _XTAL_FREQ 20000000     // Processor frequency (for delay functions)
+
+// FUNCTIONS
+
+void main(void);
