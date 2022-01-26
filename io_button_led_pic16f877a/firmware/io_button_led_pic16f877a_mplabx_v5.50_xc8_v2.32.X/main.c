deb50f91ff5167186ae8920989eefb664be30fbc
david <davidcoelho89@gmail.com>
2022-01-25 22:44:39 -0300

    Digital IO
    Description: Added Digital IO Project
    
    Commit: Add
    
    TaskNumber:#0

---
+++ io_button_led_pic16f877a/firmware/io_button_led_pic16f877a_mplabx_v5.50_xc8_v2.32.X/main.c
@@ -0,0 +1,80 @@
+/*
+ * PIC16F877A Samples
+ * Sample 03: IO ports
+ * Author: David Nascimento Coelho
+ * Last Update: 2022/01/25
+ */
+
+// LIBRARIES INCLUDE
+
+#include <xc.h>
+#include "main.h"
+
+// FUNCTIONS
+
+/* Set all Needed registers */
+void init(void)
+{
+// I/O
+    PORTA = 0b00000000;
+    PORTB = 0b00000000;
+    PORTC = 0b00000000;
+    TRISD = 0b00000000;
+    PORTE = 0b00000000;
+
+    TRISA = 0b00000000;
+    TRISB = 0b00000001;
+    TRISC = 0b00000000;
+    PORTD = 0b00000000;
+    TRISE = 0b00000000;
+}
+
+void blink_led(void)
+{
+    led1 = 1;
+    __delay_ms(500);
+    led1 = 0;
+    __delay_ms(500);         
+}
+
+void blink_if_button(void)
+{
+    if(bot1)
+    {
+        led1 = 1;
+        __delay_ms(500);
+        led1 = 0;
+        __delay_ms(500);
+    }
+    else
+    {
+        led1 = 0;
+    }
+}
+
+void button_trigger(void)
+{
+    if(bot1)
+    {
+        led1 = 1;
+        __delay_ms(2000);
+        led1 = 0;
+    }
+}
+
+void main(void) 
+{
+    // initialization routine
+    init();
+    
+    // Infinity loop
+    while(1)
+    {
+        /* Choose one of the functions below*/
+        blink_led();
+        //blink_if_button();
+        //button_trigger();
+    }
+    
+    return;
+}
---
+++ io_button_led_pic16f877a/firmware/io_button_led_pic16f877a_mplabx_v5.50_xc8_v2.32.X/main.h
@@ -0,0 +1,39 @@
+/*
+ * PIC16F877A Samples
+ * Sample 03: IO ports
+ * Author: David Nascimento Coelho
+ * Last Update: 2022/01/25
+ */
+
+// DEFINE ".h" JUST ONE TIME
+
+#ifndef MAIN_H
+#define	MAIN_H
+#endif	/* MAIN_H */
+
+// CONFIGURATION BITS
+
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = ON         // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3/PGM pin has PGM function; low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// Another way: #pragma config WDTE = ON, FOSC = XT, CPD = ON, ...
+
+// CONSTANTS
+
+#define _XTAL_FREQ 20000000     // Processor frequency for delay functions
+
+#define bot1 !PORTBbits.RB0     // button at RB0 pin
+#define led1 PORTBbits.RB1      // led at RB1 pin
+
+// FUNCTIONS
+
+void main(void);
+void blink_led(void);
+void blink_if_button(void);
+void button_trigger(void);
